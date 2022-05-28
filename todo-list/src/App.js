3608f0a57c6471f4db0b458f20a3794571dee180
kolya <Amautor@inbox.ru>
2022-05-28 23:18:02 +0300

    step-3

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -1,37 +1,43 @@
-import React, { Component } from "react";
+import React from "react";
 
-class App extends Component {
-    render() {
-        return (
-            <div className="container">
-                <div className="row">
-                    <div className="col-12">
-                        <h1 className="title">Hello world!</h1>
-                        <ul className="list-group list-group-flush">
-                            <li className="list-group-item">
-                                <div className="form-check">
-                                    <input className="form-check-input" type="checkbox" value="" id="todo-item-check-1" />
-                                    <label className="form-check-label" htmlFor="todo-item-check-1">Take out the trash</label>
-                                </div>
-                            </li>
-                            <li className="list-group-item">
-                                <div className="form-check">
-                                    <input className="form-check-input" type="checkbox" value="" id="todo-item-check-2" />
-                                    <label className="form-check-label" htmlFor="todo-item-check-2">Buy bread</label>
-                                </div>
-                            </li>
-                            <li className="list-group-item">
-                                <div className="form-check">
-                                    <input className="form-check-input" type="checkbox" value="" id="todo-item-check-3" />
-                                    <label className="form-check-label" htmlFor="todo-item-check-3">Teach penguins to fly</label>
-                                </div>
-                            </li>
-                        </ul>
-                    </div>
+export default function App() {
+    let items = [
+        {
+            id: 1,
+            text: 'Learn Javascript',
+            completed: false
+        },
+        {
+            id: 2,
+            text: 'Learn React',
+            completed: false
+        },
+        {
+            id: 3,
+            text: 'Build a React App',
+            completed: false
+        },
+    ];
+    let title = 'Things to do';
+    return (
+        <div className="container">
+            <div className="row">
+                <div className="col-12">
+                    <h1 className="title">{title}</h1>
+                    <ul className="list-group list-group-flush">
+                        {items.map(item => {
+                            return (
+                                <li key={item.id} className="list-group-item">
+                                    <div className="form-check">
+                                        <input className="form-check-input" type="checkbox" value="" id={`todo-item-check-${item.id}`} />
+                                        <label className="form-check-label" htmlFor={`todo-item-check-${item.id}`}>{item.text}</label>
+                                    </div>
+                                </li>
+                            );
+                        })}
+                    </ul>
                 </div>
             </div>
-        );
-    }
+        </div>
+    );
 }
-
-export default App;
