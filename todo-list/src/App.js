161ce2b299161831ca5f659f8ec309d3dc92e13d
kolya <Amautor@inbox.ru>
2022-05-28 23:26:40 +0300

    step-4

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import TodoList from "./TodoList";
 
 export default function App() {
     let items = [
@@ -22,21 +23,7 @@ export default function App() {
     return (
         <div className="container">
             <div className="row">
-                <div className="col-12">
-                    <h1 className="title">{title}</h1>
-                    <ul className="list-group list-group-flush">
-                        {items.map(item => {
-                            return (
-                                <li key={item.id} className="list-group-item">
-                                    <div className="form-check">
-                                        <input className="form-check-input" type="checkbox" value="" id={`todo-item-check-${item.id}`} />
-                                        <label className="form-check-label" htmlFor={`todo-item-check-${item.id}`}>{item.text}</label>
-                                    </div>
-                                </li>
-                            );
-                        })}
-                    </ul>
-                </div>
+                <TodoList title={title} items={items} />
             </div>
         </div>
     );
