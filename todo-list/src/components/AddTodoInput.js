18cfb79c1808959dcd47e7e9e57d8a5355e347a5
kolya <Amautor@inbox.ru>
2022-06-13 12:02:24 +0300

    step-8

--- todo-list/src/components/AddTodoInput.js
+++ todo-list/src/components/AddTodoInput.js
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { useState, useCallback } from "react";
 
 export default function AddTodoInput() {
+    const [value, setValue] = useState('');
+    const handleKeyUpEvent = useCallback(e => {
+        if (e.key === 'Enter') {
+            console.log(e.key + ' pressed');
+        };
+    }, []);
+    const handleChangeEvent = useCallback(e => {
+        setValue(e.target.value);
+    }, [setValue]);
     return (
-        <input type="text" className="form-control add-todo" placeholder="Add New"/>
+        <input 
+            type="text"
+            className="form-control add-todo"
+            placeholder="Add New" 
+            onChange={handleChangeEvent} 
+            onKeyUp={handleKeyUpEvent}
+            value={value}
+        />
     );
 }
